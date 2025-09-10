901d6f25de8d8fc05c70076d70b85ad601156191
alexandretonarqui <alexandre.tonarqui@gmail.com>
2025-09-10 19:04:08 -0300

    Obtendo um usuário por id

---
+++ workshopmongo/src/main/java/com/aletonarqui/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.aletonarqui.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}	
+}
